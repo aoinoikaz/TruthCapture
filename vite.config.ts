71040d1625f4a8e25b997761a97002bebf771d91
Dev <devontomlin97@gmail.com>
2025-07-05 14:21:09 -0400

    fix tailwind annoyin gvite lol

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3001,  // Different port from GB Hub
-    host: true,
+  base: '/',
+  build: {
+    outDir: 'dist',
   },
-  preview: {
+  server: {
     port: 3001,
-    host: true,
-  }
-})
+    strictPort: true,
+    host: true
+  },
+  plugins: [
+    tailwindcss(),
+  ],
+});
